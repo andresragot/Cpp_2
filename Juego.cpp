90952f3dac749e985da0c326b4edba3a4c4c112f
Andrés Ragot <andresragot99@gmail.com>
2024-10-02 16:44:47 +0200

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -2,19 +2,15 @@
 
 
 
-Juego::Juego(){
-	//tampoco supe como declarar clases dentro de un constructor cuando ya la variable la tenemos en el header.
+Juego::Juego()
+{
 	this-> m1();
 	Mosca mosqui(m1.get_x(), m1.get_y(), m1.get_z());
 	Jugador player();
 	Jugar();
 }
 
-void Juego::Jugar(){
-	//aqui va la condicion de ganar que si la logra agarrar a o no y adentro se verifica si estan en la misma posicion
-	/*while(booleano dependiendo del player atrapada){
-	verificar si estan en la misma posisicion,
-	correr el atrapar del player y darle el nuevo valor a el booleano de arriba.
-	}
-	*/
+void Juego::Jugar()
+{
+	
 }
