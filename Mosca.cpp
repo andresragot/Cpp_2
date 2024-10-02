fe5ca6fbaea49d1fc659f4353bba7cc84dabd7e4
Andrés Ragot <andresragot99@gmail.com>
2024-10-02 16:46:04 +0200

    Update Mosca.cpp

--- Mosca.cpp
+++ Mosca.cpp
@@ -5,7 +5,8 @@
 
 Mosca::Mosca() : x{0}, y{0}, z{0}{}
 
-Mosca::Mosca(int _x, int _y, int _z) : x{0}, y{0}, z{0}{
+Mosca::Mosca(int _x, int _y, int _z) : x{0}, y{0}, z{0}
+{
 	Mover(_x, _y, _z);
 }
 
@@ -13,14 +14,16 @@ int Mosca::get_x() const {return x;}
 int Mosca::get_y() const {return y;}
 int Mosca::get_z() const {return z;}
 
-Mosca& Mosca::set(const int _x,const int _y, const int _z) {
+Mosca& Mosca::set(const int _x,const int _y, const int _z) 
+{
 	this->x = _x;
 	this->y = _y;
 	this->z = _z;
 	return *this;
 }
 
-void Mosca::Mover(int m_x, int m_y, int m_z){
+void Mosca::Mover(int m_x, int m_y, int m_z)
+{
 	srand((unsigned) time(NULL));
 	const int a = rand() % (m_x+1);
 	const int b = rand() % (m_x+1);
